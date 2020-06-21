ef04a540ffcac76b1ff7cc706a88a24ca6a58a4e
omal09 <omalstephan@gmail.com>
2020-06-22 03:01:55 +0530

    js assignment

---
+++ site/module4-solution/SpeakGoodBye.js
@@ -0,0 +1,10 @@
+//*(function(window){
+//	var byeSpeaker = {};
+//	byeSpeaker.name = speak
+//	var speakWord = "Good Bye";
+//(function speak(name) {
+  //console.log(speakWord + " " + name);
+//}
+//window.byeSpeaker = byeSpeaker;
+//}(window);//
+
---
+++ site/module4-solution/SpeakHello.js
@@ -0,0 +1,9 @@
+//(function(window){
+//	var helloSpeaker = {};
+//	helloSpeaker.name = speak;
+//	var speakWord = "Hello";
+//function speak(name) {
+  //console.log(speakWord + " " + name);
+//}
+//window.helloSpeaker = helloSpeaker
+//})(window);
